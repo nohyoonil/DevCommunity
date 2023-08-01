b8a06d1e8eacbe235e7d5f894608cf6c64b737bf
yuun-dev <yuun.dev@gmail.com>
2023-08-01 14:25:03 +0900

    feature: User Entity 생성 및 h2 db 설정

---
+++ src/main/java/com/yoon/devcommunity/entity/User.java
@@ -0,0 +1,30 @@
+package com.yoon.devcommunity.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    private String password;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime deletedDate;
+}
